4d26343e65d8392516fb07e73c1ae6b83690edf9
Joan Creus-Costa <jcreus@stanford.edu>
2019-06-27 21:25:11 -0700

    unified printf to save RAM via janky implementation

--- core/serial.c
+++ core/serial.c
@@ -2,14 +2,23 @@
 
 #if USE_DEBUG_UART
 
+#include "semphr.h"
 #include "serial.h"
 #include "pwm.h"
 
 #include "stream_buffer.h"
 
+SemaphoreHandle_t serial_mutex;
+SemaphoreHandle_t printing_mutex;
+SemaphoreHandle_t postprint_mutex;
 StreamBufferHandle_t serial_stream;
 char printf_buffer[512];
 uart_t debug_uart;
+TaskHandle_t serial_handle = 0;
+
+char *printing_task;
+const char *printing_fmt;
+va_list printing_va;
 
 void raw_print(const char *fmt, ...) {
     va_list va;
@@ -20,64 +29,61 @@ void raw_print(const char *fmt, ...) {
 }
 
 void print(const char *fmt, ...) {
-    va_list va;
-    va_start(va, fmt);
-    taskENTER_CRITICAL();
-    char *tn = pcGetCurrentTaskName();
-    //char *tn = "hello!";
-    int ticks = xTaskGetTickCount()/1000;
-    int mins = ticks/60;
-    int secs = ticks%60;
-    int nc = snprintf_(printf_buffer, 21, "<%6s|%2d:%02d> ", tn, mins, secs);
-    int nb = vsnprintf_(printf_buffer + nc, 512 - nc, fmt, va);
-    xStreamBufferSend(serial_stream, printf_buffer, nb + nc, 0);
-    taskEXIT_CRITICAL();
+    xSemaphoreTake(serial_mutex, portMAX_DELAY);
+    printing_task = pcGetCurrentTaskName();
+    printing_fmt = fmt;
+    va_start(printing_va, fmt);
+    
+    xSemaphoreGive(printing_mutex);
+    xSemaphoreTake(postprint_mutex, portMAX_DELAY);
+    va_end(printing_va);
+    xSemaphoreGive(serial_mutex);
 }
 
 void serial_task(void *params){
-    uint8_t usb_buffer[64];
-    vTaskDelay(500);
     while (true) {
-        int recv = xStreamBufferReceive(serial_stream, usb_buffer, 64, 200);
-        if (recv > 0) {
-            uart_write(&debug_uart, usb_buffer, recv);
-        }
+        xSemaphoreTake(printing_mutex, portMAX_DELAY);
+        int ticks = xTaskGetTickCount()/1000;
+        int mins = ticks/60;
+        int secs = ticks%60;
+        int nc = snprintf_(printf_buffer, 21, "<%6s|%2d:%02d> ", printing_task, mins, secs);
+        int nb = vsnprintf_(printf_buffer + nc, 512 - nc, printing_fmt, printing_va);
+        uart_write(&debug_uart, (uint8_t*)printf_buffer, nc + nb);
+        xSemaphoreGive(postprint_mutex);
     }
 }
 
 #if USAGE_REPORT
 
-TaskStatus_t pxTaskStatusArray[16];
+TaskStatus_t task_statuses[16];
 
 int base = 0;
 
 void usage_task(void *params) {
-    int n = 0;
-    volatile UBaseType_t uxArraySize, x;
-    unsigned long ulTotalRunTime, ulStatsAsPercentage;
+    volatile UBaseType_t arr_size;
+    unsigned long total_runtime;
+    vTaskDelay(USAGE_REPORT_INITIAL_WAIT);
     while (true) {
         if (used_tcs[USAGE_REPORT_TC]) {
             print("ERROR ERROR IMPORTANT ERROR ERROR\n");
             print("The timer counter used for the usage report is taken by a PWM pin\n");
             print("Change USAGE_REPORT_TC in the config as necessary\n");
         }
-        uxArraySize = uxTaskGetSystemState( pxTaskStatusArray, 8, &ulTotalRunTime);
+        arr_size = uxTaskGetSystemState(task_statuses, 8, &total_runtime);
 
         print("RAM & CPU usage report:\n");
-        for( x = 0; x < uxArraySize; x++ ) {
-            uint32_t num = TC4->COUNT32.COUNT.reg;
+        for (int x = 0; x<arr_size; x++) {
             float pc = 0;
-            if (ulTotalRunTime != 0) {
-                pc = pxTaskStatusArray[x].ulRunTimeCounter*100.;
-                pc /= ulTotalRunTime;
+            if (total_runtime != 0) {
+                pc = task_statuses[x].ulRunTimeCounter*100.;
+                pc /= total_runtime;
             }
-            print("%8s - %3d spare words- %.1f%%\n",
-                pxTaskStatusArray[ x ].pcTaskName,
-                pxTaskStatusArray[ x ].usStackHighWaterMark,
+            print("%8s - %3d spare words - %.1f%%\n",
+                task_statuses[x].pcTaskName,
+                task_statuses[x].usStackHighWaterMark,
                 pc);
         }
-        print("base is %d\n", base);
-        vTaskDelay(2000);
+        vTaskDelay(USAGE_REPORT_INTERVAL);
     }
 }
 
@@ -98,7 +104,7 @@ void
 () {
     TcCount16 *hw = (TcCount16*)(((char*)TCC0) + 1024 * USAGE_REPORT_TC);
     base += 65536;
-    hw->INTFLAG.bit.OVF = 1;
+    hw->INTFLAG.reg |= TC_INTFLAG_OVF;
 }
 
 inline uint32_t vGetRunTimeCounterValue(void) {
@@ -120,22 +126,9 @@ void vConfigureTimerForRunTimeStats(void) {
     hw->CTRLA.bit.WAVEGEN = 0x0; // normal frequency
     hw->CTRLA.bit.PRESCALER = 0x6; // div256
     hw->INTENSET.bit.OVF = 1;
-    //hw->CC[0].reg = 65537;
     hw->CTRLA.bit.ENABLE = 1;
     while (hw->STATUS.bit.SYNCBUSY);
     NVIC_EnableIRQ(TCC0_IRQn + USAGE_REPORT_TC);
-/*
-    REG_GCLK_CLKCTRL = (uint16_t) (GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_ID_TC4_TC5) ;
-
-    TcCount32* TC = (TcCount32*) TC4;
-    TC->CTRLA.reg |= TC_CTRLA_MODE_COUNT32;
-    TC->CTRLA.reg |= TC_CTRLA_WAVEGEN_NFRQ;
-    TC4->COUNT32.CC[0].reg = 4294967295;
-    //TC4->COUNT32.CC[0].reg = 150000;
-    TC->CTRLA.reg |= TC_CTRLA_PRESCALER_DIV256;
-    TC->CTRLA.reg |= TC_CTRLA_ENABLE;
-    while (TC4->COUNT32.STATUS.bit.SYNCBUSY);
-*/
 
 }
 
@@ -143,15 +136,18 @@ void vConfigureTimerForRunTimeStats(void) {
 #endif
 
 void init_serial() {
-    //uart_init(&debug_uart, 2, 115200, PA12, PINMUX_PA12C_SERCOM2_PAD0, PA13, PINMUX_PA13C_SERCOM2_PAD1);
     uart_init(&debug_uart, DEBUG_UART_SERCOM, DEBUG_UART_BAUD,
                 DEBUG_UART_TX_PIN, DEBUG_UART_TX_MUX,
                 DEBUG_UART_RX_PIN, DEBUG_UART_RX_MUX);
 
     serial_stream = xStreamBufferCreate(512, 64);
-    xTaskCreate(serial_task, "serial", 100, 0, 5, NULL);
+    serial_mutex = xSemaphoreCreateBinary();
+    xSemaphoreGive(serial_mutex);
+    printing_mutex = xSemaphoreCreateBinary();
+    postprint_mutex = xSemaphoreCreateBinary();
+    xTaskCreate(serial_task, "serial", 130, 0, 5, &serial_handle);
     #if USAGE_REPORT
-        xTaskCreate(usage_task, "usage", 150, 0, 5, NULL);
+        xTaskCreate(usage_task, "usage", 80, 0, 5, NULL);
     #endif
 }
 
