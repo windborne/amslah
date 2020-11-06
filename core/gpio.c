6f0e08ea65264a94570e2ccd399fcd2a8c88f045
Michal <mikh3x4@gmail.com>
2020-11-05 17:39:20 -0800

    seperated gpio pulls and dir

--- core/gpio.c
+++ core/gpio.c
@@ -4,11 +4,16 @@
 
 #include "gpio.h"
 
-void gpio_direction(uint8_t pin, enum gpio_direction direction){
-    gpio_direction_and_pull(pin, direction, GPIO_PULL_OFF);
+void gpio_pull(uint8_t pin, enum gpio_pull_mode pull_mode) {
+    if(pin==NOT_A_PIN) return;
+
+    if(pull_mode == GPIO_PULL_ON)
+        PORT_IOBUS->Group[GPIO_PORT(pin)].PINCFG[GPIO_PIN(pin)].bit.PULLEN = true;
+    else
+        PORT_IOBUS->Group[GPIO_PORT(pin)].PINCFG[GPIO_PIN(pin)].bit.PULLEN = false;
 }
 
-void gpio_direction_and_pull(uint8_t pin, enum gpio_direction direction, enum gpio_pull_mode pull_mode) {
+void gpio_direction(uint8_t pin, enum gpio_direction direction) {
     if(pin==NOT_A_PIN) return;
     if (direction == GPIO_DIRECTION_OUT) {
         uint32_t pinsel = 1U << GPIO_PIN(pin);
@@ -22,7 +27,7 @@ void gpio_direction_and_pull(uint8_t pin, enum gpio_direction direction, enum gp
         uint32_t pinsel = 1U << GPIO_PIN(pin);
         PORT_IOBUS->Group[GPIO_PORT(pin)].DIRCLR.reg = pinsel;
 
-        uint32_t pull_mask = pull_mode == GPIO_PULL_ON ? PORT_WRCONFIG_PULLEN : 0;
+        uint32_t pull_mask = PORT_IOBUS->Group[GPIO_PORT(pin)].PINCFG[GPIO_PIN(pin)].bit.PULLEN ? PORT_WRCONFIG_PULLEN : 0;
 
         critical_section_enter();
         PORT_IOBUS->Group[GPIO_PORT(pin)].WRCONFIG.reg = PORT_WRCONFIG_WRPINCFG | pull_mask | PORT_WRCONFIG_INEN | (pinsel & 0xffff);
@@ -63,12 +68,14 @@ void digital_out_init(uint8_t pin) {
 
 void digital_in_init(uint8_t pin) {
     gpio_function(pin, GPIO_FUNCTION_OFF);
-    gpio_direction_and_pull(pin, GPIO_DIRECTION_IN, GPIO_PULL_OFF);
+    gpio_pull(pin, GPIO_PULL_OFF);
+    gpio_direction(pin, GPIO_DIRECTION_IN);
 }
 
 void digital_in_pull_init(uint8_t pin) {
     gpio_function(pin, GPIO_FUNCTION_OFF);
-    gpio_direction_and_pull(pin, GPIO_DIRECTION_IN, GPIO_PULL_ON);
+    gpio_pull(pin, GPIO_PULL_ON);
+    gpio_direction(pin, GPIO_DIRECTION_IN);
 }
 
 uint8_t digital_get(uint8_t pin) {
