869cb3193d20e2261844ba3f0c1017aeab9a30e3
Joan Creus-Costa <jcreus@stanford.edu>
2020-11-22 16:52:30 -0800

    uart was disabled on sammy51 lol

--- core/uart.c
+++ core/uart.c
@@ -75,9 +75,9 @@ void uart_init(uart_t *uart, int sercom, int baud, uint8_t pin_tx, uint32_t mux_
     hw->USART.CTRLB.bit.SBMODE = 0; // One stop bit
     hw->USART.BAUD.reg = _uart_get_baud_reg(baud);
 	hw->USART.CTRLB.bit.ENC = 0;
-    hw->USART.CTRLB.bit.RXEN = 0; // Receiver
+    hw->USART.CTRLB.bit.RXEN = 1; // Receiver
     hw->USART.CTRLB.bit.TXEN = 1; // Transmitter
-    //hw->USART.INTENSET.reg = SERCOM_USART_INTENSET_RXC;
+    hw->USART.INTENSET.reg = SERCOM_USART_INTENSET_RXC;
     hw->USART.CTRLA.reg |= 1 << SERCOM_USART_CTRLA_ENABLE_Pos; // Enable
 
     while (hw->USART.SYNCBUSY.bit.CTRLB);
