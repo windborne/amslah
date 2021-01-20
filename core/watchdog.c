1673d10a77fd5b7c2035d48148361004dd170663
John Dean <deanjl@stanford.edu>
2021-01-20 11:01:42 -0800

    watchdog warnings

--- core/watchdog.c
+++ core/watchdog.c
@@ -4,14 +4,15 @@ void watchdog_init(int period) {
 	GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID_WDT |
 						GCLK_CLKCTRL_CLKEN |
 						GCLK_CLKCTRL_GEN_GCLK3;
-	NVIC_DisableIRQ(WDT_IRQn);
+	NVIC_EnableIRQ(WDT_IRQn);
 
 	WDT->CTRL.reg = 0;
 	while (WDT->STATUS.bit.SYNCBUSY);
 
-	WDT->INTENCLR.bit.EW = 1;
 	WDT->CONFIG.bit.PER = period;
-	WDT->CTRL.bit.WEN = 0;
+	WDT->CTRL.bit.WEN = 0;							
+	WDT->INTENSET.bit.EW = 1;							//enable warning interrupt
+	WDT->EWCTRL.bit.EWOFFSET = period-1;  //set period to half that of the reset interval
 	while (WDT->STATUS.bit.SYNCBUSY);
 
 	WDT->CTRL.bit.ENABLE = 1;
